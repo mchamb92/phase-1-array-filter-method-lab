ef090cc409f10cb0189083e30edff1dbcbe6272e
Mustafa <mchambqa@gmail.com>
2021-08-11 08:42:00 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(collection, match){
+    const filteredCollection = collection.filter((element) => {
+        if (element.toLowerCase() === match.toLowerCase()){
+            return element;
+        }
+    })
+    return filteredCollection;
+}
+
+function fuzzyMatch(collection, match){
+    const matchLength = match.split('').length;
+    const filteredCollection = collection.filter((element) => {
+      const elementSlice = element.split('').slice(0, matchLength).join('');
+        if( elementSlice === match){
+            return element;
+        }
+    })
+    return filteredCollection;
+}
+
+function matchName(collection, match){
+    const filteredCollection = collection.filter((element) => {
+        if(element.name === match){
+            return element;
+        }
+    })
+    return filteredCollection;
+}
